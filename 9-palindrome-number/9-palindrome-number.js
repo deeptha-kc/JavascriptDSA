7c3a90d3520debf6dfc71c65eccc759c8709d42d
Deepthainfo <88706147+Deepthainfo@users.noreply.github.com>
2022-06-19 18:55:26 +0530

    Time: 276 ms (42.63%), Space: 50.6 MB (81.54%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -3,23 +3,22 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    var rem, temp, final = 0;
-		var number = x
-
-		temp = number;
-		while(number>0)
-		{
-			rem = number%10;
-			number = parseInt(number/10);
-			final = final*10+rem;
-		}
-		if(final==temp)
-		{
-			return true
-		}
-		else
-		{
-			return false
-		}
+    var rem, temp, q=0;
+    let number = x;
     
+    temp = number;
+    
+    while (number > 0)
+        {
+            rem= number%10;
+            number=parseInt(number/10);
+            q=q*10+rem;
+        }
+    
+    if(temp==q){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
