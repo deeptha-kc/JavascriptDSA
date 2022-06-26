fc489f61079cfc41ef1f481e33a5d15be1d6ba13
deeptha-kc <88706147+deeptha-kc@users.noreply.github.com>
2022-06-26 21:40:39 +0530

    Time: 69 ms (89.66%), Space: 42.3 MB (69.16%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.js
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -4,11 +4,11 @@
  */
 var longestCommonPrefix = function(strs) {
     if (strs.length === 0) return "";
-    let str = strs[0];
-    for (let word of strs) {
-        while (word.indexOf(str) !== 0) {
-            str = str.slice(0, str.length - 1);
-        }
+    let prefix=strs[0];
+    
+    for(let i=0;i<strs.length;i++){
+        while( strs[i].indexOf(prefix)!=0)
+            prefix=prefix.substring(0,prefix.length -1 )       
     }
-    return str;
-};
+    return prefix
+};  
