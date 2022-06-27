5cc4856627e4c227ee84a302b3a38aaadc87fe74
deeptha-kc <88706147+deeptha-kc@users.noreply.github.com>
2022-06-27 21:37:35 +0530

    Time: 207 ms (44.33%), Space: 53.4 MB (58.89%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    let result = {};
+for (let word of strs) {
+    let cleansed = word.split("").sort().join("");
+    if (result[cleansed]) {
+      result[cleansed].push(word);
+    } else {
+      result[cleansed] = [word];
+    }
+  }
+return Object.values(result);
+};
