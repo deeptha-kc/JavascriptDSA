af94e0b269baf97a28f7404738a05875f7797052
deeptha-kc <88706147+deeptha-kc@users.noreply.github.com>
2022-06-28 20:43:36 +0530

    Time: 115 ms (38.93%), Space: 44.3 MB (67.43%) - LeetHub

---
+++ 268-missing-number/268-missing-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    let numsSum = 0;
+    let corrSum = 0;
+    for (let i = 0; i < nums.length + 1; i++) {
+        corrSum += i;
+        numsSum += i < nums.length ? nums[i] : 0;
+    }
+    return corrSum - numsSum;
+};
