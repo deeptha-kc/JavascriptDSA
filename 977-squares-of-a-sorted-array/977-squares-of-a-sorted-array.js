0a86595c3283e67a2e16eed83e57afe7306ffb2c
deeptha-kc <88706147+deeptha-kc@users.noreply.github.com>
2022-07-02 20:32:08 +0530

    Time: 118 ms (82.01%), Space: 48.4 MB (86.09%) - LeetHub

---
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+    let squaredNums = nums.map(function(a) {
+       return a * a;
+    }).sort(function(a, b) {
+       return a - b;
+    });
+    return squaredNums;
+};
